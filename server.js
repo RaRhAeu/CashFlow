707b80a546a0e45ea476f1a735571d3812c8005a
Radzim Kowalow <radzim_ko@wp.pl>
2020-01-03 03:53:34 +0100

    configuration change

--- server.js
+++ server.js
@@ -9,23 +9,28 @@ const payments = require('./routes/api/payments');
 
 const app = express();
 
-// body parser
+// body parser middleware
 app.use(bodyParser.json());
 
 // db config
-
 const db = require('./config/keys').mongoURI;
-// connect to mongo
+// connection to mongodb
 mongoose.connect(db)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
 
 // routing
 app.use('/api/events', events);
 app.use('/api/expenses', expenses);
 app.use('/api/payments', payments);
 
-const port = process.env.PORT || 5000
+// static files
+// app.use(express.static(__dirname + '/dist'))
+
+const port = process.env.PORT || 5000;
+
+// prod env settings
+// app.disable('x-powered-by');
 
 app.listen(port, () => console.log(
   `Server started on port: ${port}`
