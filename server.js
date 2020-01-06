cc0a2bf92129e0c032a52d50285d060959a5302b
Radzim Kowalow <radzim_ko@wp.pl>
2020-01-06 12:23:13 +0100

    Version 1.0.0

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/payments', payments);
 
 // static files
 // app.use(express.static('client/build'))
-if (true) {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
